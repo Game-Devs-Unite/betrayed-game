db03ed74cf7905a5a94453c24aabf4b37fcefdcb
aditya <aarshisback@gmail.com>
2020-11-02 22:19:43 +0530

    flattening history

---
+++ README.md
@@ -0,0 +1,16 @@
+# Betrayed
+This game is a work in progress.
+
+# Building
+NOTE: Tested only on Ubuntu
+
+## On Linux, for Linux
+Dependencies: `g++`, `make`, `libSDL2-dev` \
+Run `make` in the project root. \
+The built binary will be stored as `bld/Betrayed.out`
+
+## On Linux, for Windows
+Dependencies: `g++-mingw-w64-x86-64`, `make` \
+Run `make windows` in the project root. \
+The built binary will be stored as `bld/Betrayed.exe`. \
+Run it under Windows or using `wine`.
---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <SDL2/SDL.h>
+#include <cstdio>
+
+int main(int argc, char **argv) {
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("ERROR: could notta initialize SDL.\n");
+    return -1;
+  }
+
+  SDL_Window *w =
+      SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_VULKAN);
+
+  if (!w) {
+    printf("ERROR: could not create SDL window\n");
+    return -1;
+  }
+
+  bool quit = false;
+  SDL_Event e;
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        quit = true;
+    }
+  }
+
+  return 0;
+}
